438f60e9697abfe57ac2f0c77604c45ee16ab586
Ishita Pareek <143587484+ishitapareek@users.noreply.github.com>
2026-05-01 19:02:29 +0530

    file cleaned

--- Code/db.py
+++ Code/db.py
@@ -4,7 +4,7 @@ def get_db():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="admin",
+        password="*****",
         database="Cruise_Reservation_System"
     )
 
